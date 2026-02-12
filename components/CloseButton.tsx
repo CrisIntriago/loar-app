44186bc4474d2054ce92948cb59e88ed3b366830
CrisIntriago <caintria@espol.edu.ec>
2026-02-12 13:08:06 -0500

    feat: handleclose

--- components/CloseButton.tsx
+++ components/CloseButton.tsx
@@ -13,6 +13,7 @@ export default function CloseButton({ executionId }: { executionId?: string }) {
                 }),
             }).catch(err => console.error('Jelou callback failed:', err));
         }
+        window.close();
     };
 
     return (
